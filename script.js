30d770a13aaee9c684ec1a4251ef186eb6049062
pebrii30 <adepebriana664@gmail.com>
2024-12-11 09:05:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector('.header .navbar');
+
+
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+    navbar.classList.remove('active');
+}
